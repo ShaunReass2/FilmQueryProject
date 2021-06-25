9ba4b28ebb2e329785f7643b99a7857f738d0ae7
Shaun R <shaun.reass@icloud.com>
2021-06-25 15:04:46 -0500

    initial commit of source

---
+++ src/com/skilldistillery/filmquery/entities/Actor.java
@@ -0,0 +1,19 @@
+package com.skilldistillery.filmquery.entities;
+
+public class Actor {
+	private int id; 
+	private String firstName; 
+	private String lastName; 
+	
+	public Actor() { }
+	
+	public Actor(String firstName, String lastName) {
+		super(); 
+		this.firstName = firstName;
+		this.lastName = lastName; 
+	}
+	
+	
+}
+
+	
